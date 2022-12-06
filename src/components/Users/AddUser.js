31257e13d05875fd97e368b6869e3d8a428f7575
Aleksandre Tcharbadze <riseofkelevra@gmail.com>
2022-12-05 20:25:30 -0500

    Added some restrictions to the inputs

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,5 +1,5 @@
 // Native imports
-import React from "react";
+import React, {useState} from "react";
 // Styling
 import classes from "./AddUser.module.css";
 // Custom components
@@ -7,17 +7,49 @@ import Card from "../UI/Card";
 import Button from "../Button/Button";
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
   const addUserHandler = (e) => {
     e.preventDefault();
+
+    // check if the inputs are empty
+    if(enteredUsername.trim().length === 0 || enteredAge.trim() === 0){
+      return;
+    }
+
+    // check if the number is lower then a certain number (in our case we'll put 1)
+    if(+enteredAge < 1){
+      return;
+    }
+
+    console.log(enteredUsername, enteredAge);
+    setEnteredUsername('');
+    setEnteredAge('');
   };
+
+  const usernameChangeHandler = (e) =>{
+    setEnteredUsername(e.target.value);
+  }
+
+  const ageChangeHandler = (e) =>{
+    setEnteredAge(e.target.value);
+  }
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input 
+            id="username" 
+            type="text"
+            value={enteredUsername} 
+            onChange={usernameChangeHandler}/>
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" />
+        <input 
+            id="age" 
+            type="number" 
+            value={enteredAge}
+            onChange={ageChangeHandler} />
         <Button 
             type="submit" 
             value="Add User"/>

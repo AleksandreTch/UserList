7d2d17f65597b89dc7a07b6fad9f7df751b67ad2
Aleksandre Tcharbadze <riseofkelevra@gmail.com>
2022-12-14 21:18:50 -0500

    Updated onAddUser logic

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -37,6 +37,8 @@ const AddUser = (props) => {
       return;
     }
     props.onAddUser(enteredName, enteredUserAge);
+    nameInputRef.current.value = '';
+    ageInputRef.current.value = '';
   };
 
   const errorHandler = () => {
